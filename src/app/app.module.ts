c223d6996f9f32530de6733eaf6ce7070be718e3
DeDuShKa12 <pasharudiuk97@gmail.com>
2023-05-27 18:27:29 +0300

    HW 50% done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,32 @@
 import { NgModule } from '@angular/core';
+import { HttpClientModule} from "@angular/common/http";
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+
+import { UsersComponent } from './components/users/users.component';
+import { UserComponent } from './components/user/user.component';
+import {AppComponent} from "./components/app.component";
+import { UserDetailsComponent } from './components/user-details/user-details.component';
+import { PostsComponent } from './components/posts/posts.component';
+import { PostComponent } from './components/post/post.component';
+import { CommentsComponent } from './components/comments/comments.component';
+import { CommentComponent } from './components/comment/comment.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsersComponent,
+    UserComponent,
+    UserDetailsComponent,
+    PostsComponent,
+    PostComponent,
+    CommentsComponent,
+    CommentComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/constants/urls.ts
@@ -0,0 +1,11 @@
+const baseURL = 'https://jsonplaceholder.typicode.com'
+
+
+const urls = {
+  users: `${baseURL}/users`,
+  posts: `${baseURL}/posts`,
+}
+
+export {
+  urls
+}
