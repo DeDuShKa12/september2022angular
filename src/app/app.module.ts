cb14bd23988c60e64ee7b215da8f54742afff1cc
DeDuShKa12 <pasharudiuk97@gmail.com>
2023-05-30 17:03:48 +0300

    HW 2 done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,30 +3,57 @@ import { HttpClientModule} from "@angular/common/http";
 import { BrowserModule } from '@angular/platform-browser';
 
 
-import { UsersComponent } from './components/users/users.component';
-import { UserComponent } from './components/user/user.component';
 import {AppComponent} from "./components/app.component";
-import { UserDetailsComponent } from './components/user-details/user-details.component';
-import { PostsComponent } from './components/posts/posts.component';
-import { PostComponent } from './components/post/post.component';
-import { CommentsComponent } from './components/comments/comments.component';
-import { CommentComponent } from './components/comment/comment.component';
+import {RouterModule, Routes} from "@angular/router";
+import { MainLayoutComponent } from './layouts/main-layout/main-layout.component';
+import { UserPageComponent } from './pages/user-page/user-page.component';
+import { PostPageComponent } from './pages/post-page/post-page.component';
+import { HeaderComponent } from './components/header/header.component';
+import { UsersComponent } from './components/user/users/users.component';
+import { UserComponent } from './components/user/user/user.component';
+import { UserDetailsComponent } from './components/user/user-details/user-details.component';
+import { UserDetailsPageComponent } from './pages/user-details-page/user-details-page.component';
+import { PostsComponent } from './components/post/posts/posts.component';
+import { PostComponent } from './components/post/post/post.component';
+import { CommentsComponent } from './components/post/comments/comments.component';
+import { CommentPageComponent } from './pages/comment-page/comment-page.component';
 
+const routes: Routes = [
+  {
+    path: '', component: MainLayoutComponent, children: [
+      {path: '', redirectTo: 'users', pathMatch: 'full'},
+      {
+        path: 'users', component: UserPageComponent, children: [
+          {path: ':id', component: UserDetailsPageComponent}
+        ]
+      },
+      {path: 'posts', component: PostPageComponent, children: [
+          {path: ':postId', component: CommentPageComponent}
+        ]}
+    ]
+  }
+]
 
 @NgModule({
   declarations: [
     AppComponent,
+    MainLayoutComponent,
+    UserPageComponent,
+    PostPageComponent,
+    HeaderComponent,
     UsersComponent,
     UserComponent,
     UserDetailsComponent,
+    UserDetailsPageComponent,
     PostsComponent,
     PostComponent,
     CommentsComponent,
-    CommentComponent
+    CommentPageComponent,
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/constants/urls.ts
+++ src/app/constants/urls.ts
@@ -1,9 +1,18 @@
 const baseURL = 'https://jsonplaceholder.typicode.com'
 
+const users = `${baseURL}/users`
+
+const posts = `${baseURL}/posts`
 
 const urls = {
-  users: `${baseURL}/users`,
-  posts: `${baseURL}/posts`,
+  users: {
+    users,
+    byId:(id:number): string => `${users}/${id}`,
+  },
+  posts: {
+    posts,
+    CommentById:(id: number): string => `${posts}/${id}/comments`
+  }
 }
 
 export {
